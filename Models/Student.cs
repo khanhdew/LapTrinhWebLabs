e2cb58eca727762b8f0d60720ef1412dc077020f
Khanh Nguyen <khanhdeptrai292004@gmail.com>
2024-09-01 22:00:57 +0700

    lab3

--- Models/Student.cs
+++ Models/Student.cs
@@ -6,10 +6,14 @@ public class Student
 {
     public int Id { get; set; }
     [Required]
+    [StringLength(100, MinimumLength = 4)]
     public string? Name { get; set; }
-    [EmailAddress]
+    [Required(ErrorMessage ="Email bắt buộc phải được nhập")]
+    [RegularExpression(@"[A-Za-z0-9._%+-]+@gmail.com", ErrorMessage = "Email không hợp lệ")]
     public string? Email { get; set; }
-    [StringLength(100, MinimumLength = 6)]
+    [StringLength(100, MinimumLength = 8)]
+    [RegularExpression(@"""^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$""
+", ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và số")]
     public string? Password { get; set; }
     [Required]
     public Branch? Branch { get; set; }
@@ -24,4 +28,6 @@ public class Student
     public IFormFile? Photo { get; set; }
     
     public string? PhotoPath { get; set; }
+    [Range(0, 10)]
+    public float? GPA { get; set; }
 }
