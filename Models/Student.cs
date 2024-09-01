b6796b22193999ba0a970d3639e616846b206ae9
Khanh Nguyen <khanhdeptrai292004@gmail.com>
2024-09-01 22:05:32 +0700

    lab3

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Lab.Models;
@@ -6,24 +7,32 @@ public class Student
 {
     public int Id { get; set; }
     [Required]
+    [DisplayName("Họ và tên")]
     [StringLength(100, MinimumLength = 4)]
     public string? Name { get; set; }
     [Required(ErrorMessage ="Email bắt buộc phải được nhập")]
+    [DisplayName("Email")]
     [RegularExpression(@"[A-Za-z0-9._%+-]+@gmail.com", ErrorMessage = "Email không hợp lệ")]
     public string? Email { get; set; }
     [StringLength(100, MinimumLength = 8)]
+    [DisplayName("Mật khẩu")]
     [RegularExpression(@"""^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$""
 ", ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và số")]
     public string? Password { get; set; }
     [Required]
+    [DisplayName("Ngành")]
     public Branch? Branch { get; set; }
     [Required]
+    [DisplayName("Giới tính")]
     public Gender? Gender { get; set; }
+    [DisplayName("Chính quy")]
     public bool IsRegular { get; set; }
     [DataType(DataType.MultilineText)]
     [Required]
+    [DisplayName("Địa chỉ")]
     public string? Address { get; set; }
     [Range(typeof(DateTime), "1/1/1980", "1/1/2010")]
+    [DisplayName("Ngày sinh")]
     public DateTime? DateOfBirth { get; set; }
     public IFormFile? Photo { get; set; }
     
